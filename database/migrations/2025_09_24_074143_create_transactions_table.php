f7e71f131908eade769a9b06d8ecdbd72ce6e264
vinoryz <brofistjr10@gmail.com>
2025-09-24 15:26:14 +0700

    buat migration baru

---
+++ database/migrations/2025_09_24_074143_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->decimal('amount', 15, 2);
+            $table->char('description', 255)->nullable()->comment('Optional description of the transaction');
+            $table->datetime('transaction_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};

5e04a31778a64e312e5aeefda44190fc3056cf2a
vinoryz <brofistjr10@gmail.com>
2025-09-28 21:41:29 +0700

    buat model transaksi dan modifikasi categorySeeder

---
+++ app/Models/Transaction.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'amount',
+        'type',
+        'date',
+        'description',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, "user_id");
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, "category_id");
+    }
+}

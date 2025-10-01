56a4b1645f37c422c764787cc3d270d0155ecc01
vinoryz <brofistjr10@gmail.com>
2025-10-01 21:39:32 +0700

    penyesuaian users migration table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id()->primary();
             $table->string('name');
             $table->string('email')->unique();
+            $table->dateTime('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();

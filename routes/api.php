27af420eaf9d26ec01aa6f2775b35e5feec0c59d
vinoryz <brofistjr10@gmail.com>
2025-11-02 20:36:19 +0700

    quickstats aman

--- routes/api.php
+++ routes/api.php
@@ -10,11 +10,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // Add transaction routes
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/transactions', [TransactionController::class, 'store']);
-    Route::get('/transactions', [TransactionController::class, 'index']);
     Route::get('/transactions/history', [TransactionController::class, 'history']); // New history endpoint
     Route::get('/transactions/monthly-stats', [TransactionController::class, 'monthlyStats']);
     Route::post('/transactions/quick-add', [TransactionController::class, 'quickAdd']);
+
+    Route::post('/transactions', [TransactionController::class, 'store']);
+    Route::get('/transactions', [TransactionController::class, 'index']);
+    
     Route::get('/transactions/{id}', [TransactionController::class, 'show']);
     Route::put('/transactions/{id}', [TransactionController::class, 'update']);
     Route::delete('/transactions/{id}', [TransactionController::class, 'destroy']);
